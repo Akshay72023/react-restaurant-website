c664b270832548af12c6e459794de55fe41df0a3
Akshay72023 <al7864745@gmail.com>
2024-01-13 20:41:30 +0530

    cart modal is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return (
     <div>
+      <Cart />
       <Header />
       <main>
         <Meals />
